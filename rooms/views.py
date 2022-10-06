5217ccf066822cd77ce302fcbe78e45d1c8e9a57
HoJin9622 <kiss0104040@gmail.com>
2022-10-06 15:00:53 +0900

    #9.3 render

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,15 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
 def see_all_rooms(request):
-    return HttpResponse("see all rooms")
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {"rooms": rooms, "title": "Hello! this title comes from django!"},
+    )
 
 
 def see_one_rooms(request, room_id, room_name):
