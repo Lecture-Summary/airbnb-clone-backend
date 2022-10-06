4dade5a30b3684827148f38c656fe897ffbb02cd
HoJin9622 <kiss0104040@gmail.com>
2022-10-06 14:45:41 +0900

    #9.2 URL Arguments

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.say_hello),
+    path("", views.see_all_rooms),
+    path("<int:room_id>/<str:room_name>", views.see_one_rooms),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
 
-def say_hello(request):
-    return HttpResponse("hello")
+def see_all_rooms(request):
+    return HttpResponse("see all rooms")
+
+
+def see_one_rooms(request, room_id, room_name):
+    return HttpResponse(f"see room with id: {room_id} {room_name}")
