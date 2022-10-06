1f006d76c32cfc32a8d38c06cbcd0649184d6ec2
HoJin9622 <kiss0104040@gmail.com>
2022-10-06 14:36:29 +0900

    #9.0 Views

--- rooms/admin.py
+++ rooms/admin.py
@@ -31,7 +31,11 @@ class RoomAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
-    search_fields = ("=owner__username",)
+    search_fields = (
+        "name",
+        "^price",
+        "=owner__username",
+    )
 
 
 @admin.register(Amenity)
