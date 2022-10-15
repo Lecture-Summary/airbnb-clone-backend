db024163874e4438c21a9ac81202e6add3598098
HoJin9622 <kiss0104040@gmail.com>
2022-10-15 15:25:57 +0900

    #11.19 Wishlists

---
+++ wishlists/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import ModelSerializer
+from rooms.serializers import RoomListSerializer
+from .models import WishList
+
+
+class WishlistSerializer(ModelSerializer):
+    rooms = RoomListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = WishList
+        fields = ("name", "rooms")
