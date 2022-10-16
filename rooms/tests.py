668146725f3cd1367a0fad3faf4bc47c8e429be2
HoJin9622 <kiss0104040@gmail.com>
2022-10-16 17:21:46 +0900

    #16.3 Create Amenity Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -6,6 +6,8 @@ class TestAmenities(APITestCase):
     NAME = "Amenity Test"
     DESC = "Amenity Description"
 
+    URL = "/api/v1/rooms/amenities/"
+
     def setUp(self):
         models.Amenity.objects.create(
             name=self.NAME,
@@ -13,7 +15,7 @@ class TestAmenities(APITestCase):
         )
 
     def test_all_amenities(self):
-        response = self.client.get("/api/v1/rooms/amenities/")
+        response = self.client.get(self.URL)
         data = response.json()
 
         self.assertEqual(response.status_code, 200, "Status code isn't 200.")
@@ -21,3 +23,21 @@ class TestAmenities(APITestCase):
         self.assertEqual(len(data), 1)
         self.assertEqual(data[0]["name"], self.NAME)
         self.assertEqual(data[0]["description"], self.DESC)
+
+    def test_create_amenity(self):
+        new_amenity_name = "New Amenity"
+        new_amenity_description = "New Amenity desc."
+        response = self.client.post(
+            self.URL,
+            data={"name": new_amenity_name, "description": new_amenity_description},
+        )
+        data = response.json()
+        self.assertEqual(response.status_code, 200, "Not 200 status code")
+        self.assertEqual(data["name"], new_amenity_name)
+        self.assertEqual(data["description"], new_amenity_description)
+
+        response = self.client.post(self.URL)
+        data = response.json()
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("name", data)
