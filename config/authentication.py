42ae4a9149b8ce0968782278a6199fe7612103ce
HoJin9622 <kiss0104040@gmail.com>
2022-10-16 15:20:42 +0900

    #15.1 Custom Authentication

---
+++ config/authentication.py
@@ -0,0 +1,16 @@
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from users.models import User
+
+
+class TrustMeBroAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        print(request.headers)
+        username = request.headers.get("Trust-Me")
+        if not username:
+            return None
+        try:
+            user = User.objects.get(username=username)
+            return (user, None)
+        except User.DoesNotExist:
+            raise AuthenticationFailed(f"No user {username}")
