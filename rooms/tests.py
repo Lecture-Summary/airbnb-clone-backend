e63c507d0ee22766aff58f1151039ad694762746
HoJin9622 <kiss0104040@gmail.com>
2022-10-16 16:53:11 +0900

    #16.2 Amenities Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from . import models
 
-# Create your tests here.
+
+class TestAmenities(APITestCase):
+    NAME = "Amenity Test"
+    DESC = "Amenity Description"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+
+    def test_all_amenities(self):
+        response = self.client.get("/api/v1/rooms/amenities/")
+        data = response.json()
+
+        self.assertEqual(response.status_code, 200, "Status code isn't 200.")
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["name"], self.NAME)
+        self.assertEqual(data[0]["description"], self.DESC)
