db37bee53459c843482240e70df625dcdb364e04
HoJin9622 <kiss0104040@gmail.com>
2022-11-16 15:13:10 +0900

    #19.0 Manual Fetching

--- config/settings.py
+++ config/settings.py
@@ -35,10 +35,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-THIRD_PARTY_APPS = [
-    "rest_framework",
-    "rest_framework.authtoken",
-]
+THIRD_PARTY_APPS = ["rest_framework", "rest_framework.authtoken", "corsheaders"]
 
 CUSTOM_APPS = [
     "common.apps.CommonConfig",
@@ -67,6 +64,7 @@ INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -166,3 +164,5 @@ REST_FRAMEWORK = {
         "config.authentication.JWTAuthentication",
     ]
 }
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
